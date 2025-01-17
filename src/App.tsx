435ef5025642ba19507078a5624b619b8df8e942
JavaTota <jomigone@gmail.com>
2025-01-17 15:41:28 +0100

    feat(App.tsx): add button in App.tsx
    Added a button in App.tsx and cleaned the code in tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-function App() {}
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Button variant={"outline"} colorPalette={"red"}>
+      Click Me
+    </Button>
+  );
+}
 
 export default App;
