97b9ede313433d730dff63203db8ffa08321bf1c
JavaTota <jomigone@gmail.com>
2025-01-17 18:16:36 +0100

    feat(App): built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Hide } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Button variant={"outline"} colorPalette={"red"}>
-      Click Me
-    </Button>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <Hide below="md">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Hide>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
