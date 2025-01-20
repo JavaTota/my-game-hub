5276fa4b41f887e7aa1eebc92be44af741800ed4
JavaTota <jomigone@gmail.com>
2025-01-20 14:37:30 +0100

    feat(PlatformIconList):added the icons to the cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,15 +1,20 @@
-import { Game } from "../Hooks/useGames";
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Game } from "../Hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
 }
+
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow={"hidden"}>
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize={"1xl"}>{game.name}</Heading>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
